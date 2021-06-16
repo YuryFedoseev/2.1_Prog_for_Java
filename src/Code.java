01e4e68f3e94da80b06abe75cdc1ca108384622f
Yury.Fedoseev <Yri95@inbox.ru>
2021-06-16 23:19:39 +0300

    Java_01

---
+++ src/Code.java
@@ -0,0 +1,8 @@
+public class Code {
+    public static void main(String [] args){
+        int balanse= 2_000_000_000;
+        int perevod= 500_000_000;
+        int total = balanse  + perevod;
+        System.out.println(total);
+    }
+}
